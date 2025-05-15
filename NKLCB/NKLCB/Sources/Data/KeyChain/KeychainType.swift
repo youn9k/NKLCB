0509d9a073a253fda4d410bf53d174a52e95a42b
youn9k <ace_lephant@naver.com>
2025-05-15 15:34:52 +0900

    키체인 추가

---
+++ NKLCB/NKLCB/Sources/Data/KeyChain/KeychainType.swift
@@ -0,0 +1,6 @@
+public enum KeychainType: String, Sendable {
+    case accessToken = "ACCESS-TOKEN"
+    case refreshToken = "REFRESH-TOKEN"
+    case accessExpiresIn = "ACCESS-EXPIRES-IN" // 토큰이 만료될 시간을 Miliseconds(timestamp)로 저장해요
+    case deviceID = "DEVICE-ID"
+}
